bcc0958a06c032b064d19772f86ec7dfbecfb5b4
Afalkenhagen <ahau_buluk@protonmail.ch>
2023-08-10 18:53:00 -0300

    chore: changes in config

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require("dotenv").config({path: "variables.env"});
+require("dotenv").config({path: ".env"});
 const conectarDB = async ()=>{
     try {
         await mongoose.connect(process.env.DB_MONGO, {
